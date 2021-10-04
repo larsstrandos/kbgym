b01b095feaf423b989f96397055b396ec3b0b847
larsstrandos <lars@strandos-ikt.no>
2021-10-04 10:23:37 +0200

    Added the Inital files for ReactJS and TailwindCSS

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="w-full bg-black h-screen"></div>
+      <div className="w-full bg-green-500 h-screen"></div>
+    </div>
+  );
+}
+
+export default App;
