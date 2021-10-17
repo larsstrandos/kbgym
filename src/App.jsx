8f0daac0cfb8e6c772e35f97d928cdf51ba33f49
larsstrandos <lars@strandos-ikt.no>
2021-10-18 00:31:56 +0200

    Some more work

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className="flex flex-col h-screen justify-between">
         <Header />
-        <Switch>
-          <Route path="/" exact>
-            Home
-          </Route>
-          <Route path="/om-oss/" exact>
-            Om oss
-          </Route>
-          <Route path="/pt/" exact>
-            PT
-          </Route>
-        </Switch>
+        <main class="mb-auto">
+          <Switch>
+            <Route path="/" exact>
+              Home
+            </Route>
+            <Route path="/om-oss/" exact>
+              Om oss
+            </Route>
+            <Route path="/pt/" exact>
+              PT
+            </Route>
+          </Switch>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,55 +9,46 @@ const Header = () => {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <nav className="flex items-center bg-gray-800 p-1 flex-wrap">
-      <Link to="/">
-        <img src={Logo} alt="kb gym sande logo" className="h-28" />
-      </Link>
-      <button
-        onClick={() => setNavbar(!navbar)}
-        className="text-white inline-flex p-3 hover:bg-gray-900 rounded md:hidden ml-auto"
-      >
-        <FontAwesomeIcon icon={faBars} />
-      </button>
-      <div
-        className={
-          "top-nav w-full md:inline-flex md:flex-grow md:w-auto " +
-          (navbar ? "flex-inline" : "hidden")
-        }
-      >
-        <div className="md:inline-flex md:flex-row md:ml-auto flex flex-col">
-          <Link
-            to="/"
-            className="md:inline-flex md:w-auto px-3 py-2 rounded text-gray-400 hover:text-white hover:bg-gray-900"
-          >
-            <span>Heim</span>
-          </Link>
-          <Link
-            to="/om-oss/"
-            className="md:inline-flex md:w-auto px-3 py-2 rounded text-gray-400 hover:text-white hover:bg-gray-900"
-          >
-            <span>Om oss</span>
-          </Link>
-          <Link
-            to="/pt/"
-            className="md:inline-flex md:w-auto px-3 py-2 rounded text-gray-400 hover:text-white hover:bg-gray-900"
-          >
-            <span>PT</span>
-          </Link>
+    <nav className="bg-gray-800">
+      <div className="container mx-auto flex items-center p-1 flex-wrap">
+        <Link to="/">
+          <img src={Logo} alt="kb gym sande logo" className="h-28" />
+        </Link>
+        <button
+          onClick={() => setNavbar(!navbar)}
+          className="text-white inline-flex p-3 hover:bg-gray-900 rounded md:hidden ml-auto"
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
+        <div
+          className={
+            "top-nav w-full md:inline-flex md:flex-grow md:w-auto " +
+            (navbar ? "flex-inline" : "hidden")
+          }
+        >
+          <div className="md:inline-flex md:flex-row md:ml-auto flex flex-col">
+            <Link
+              to="/"
+              className="md:inline-flex md:w-auto px-3 py-2 rounded text-gray-300 hover:text-white hover:bg-gray-900 uppercase"
+            >
+              <span>Heim</span>
+            </Link>
+            <Link
+              to="/om-oss/"
+              className="md:inline-flex md:w-auto px-3 py-2 rounded text-gray-300 hover:text-white hover:bg-gray-900 uppercase"
+            >
+              <span>Om oss</span>
+            </Link>
+            <Link
+              to="/pt/"
+              className="md:inline-flex md:w-auto px-3 py-2 rounded text-gray-300 hover:text-white hover:bg-gray-900 uppercase"
+            >
+              <span>PT</span>
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
-    /*<ul className="float-right flex">
-        <li>
-          <Link to="/">Heim</Link>
-        </li>
-        <li>
-          <Link to="/om-oss/">Om oss</Link>
-        </li>
-        <li>
-          <Link to="/pt/">PT</Link>
-        </li>
-      </ul>*/
   );
 };
 
