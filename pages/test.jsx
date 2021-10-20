b59181d348016f288cfc0727e29fa86143d814e2
larsstrandos <lars@strandos-ikt.no>
2021-10-20 22:04:45 +0200

    Moving to Next.js

---
+++ pages/test.jsx
@@ -0,0 +1,13 @@
+import { FaFacebook } from "react-icons/fa";
+
+function test() {
+  return (
+    <div>
+      <FaFacebook />
+      <div className="h-5 w-5 bg-red-700"></div>
+      <p className="title">Some fucking Title</p>
+    </div>
+  );
+}
+
+export default test;
