b877822d4ceea602aa35f0e0822c49af842a788e
larsstrandos <lars@strandos-ikt.no>
2021-10-25 02:32:24 +0200

    Done some progress on the footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,29 +1,59 @@
 import Link from "next/link";
-import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 
 function footer() {
   return (
-    <footer className="w-full py-10 bg-gray-600">
+    // <link rel="stylesheet" id="monsterrat_font-css" href="https://fonts.googleapis.com/css?family=Montserrat%3A300%2C300i%2C400%2C500%2C600&amp;display=swap&amp;ver=5.8.1" type="text/css" media="all">
+    <footer className="mt-auto w-full py-10 px-2 bg-gray-600 ">
+      {/* Social Media Icons */}
       <div className="container mx-auto text-center">
-        <div className="flex justify-center">
+        <div className="flex flex-1 justify-center mb-5">
           <Link href="https://www.facebook.com/KBgymSande">
-            <a>
-              <FaInstagram className="" />
+            <a className="bg-fb text-xl text-white mx-1.5 p-2.5  rounded-full shadow-sm">
+              <FaFacebookF />
             </a>
           </Link>
           <Link href="https://www.instagram.com/kbgymsande/">
-            <a>
-              <FaFacebook />
+            <a className="bg-ig text-xl text-white mx-1.5 p-2.5  rounded-full shadow-sm">
+              <FaInstagram />
+            </a>
+          </Link>
+          <Link href="mailto:kbgym@outoo.com">
+            <a className="bg-gray-800 text-xl text-white mx-1.5 p-2.5  rounded-full shadow-sm hover:opacity-80">
+              <HiOutlineMail />
             </a>
           </Link>
         </div>
-        <p className="text-xs text-gray-100">
+
+        {/* Footer Menu */}
+
+        <div className="flex flex-1 justify-center uppercase mb-5">
+          <Link href="/om-senteret/">
+            <a className="px-1.5 text-sm text-white font-bold hover:text-opacity-90">
+              Om Senteret
+            </a>
+          </Link>
+          <Link href="/medlemskap/">
+            <a className="px-1.5 text-sm text-white font-bold hover:text-opacity-90">
+              Medlemskap
+            </a>
+          </Link>
+          <Link href="/pt/">
+            <a className="px-1.5 text-sm text-white font-bold hover:text-opacity-90">
+              PT
+            </a>
+          </Link>
+        </div>
+
+        {/* Copyright and Made by Section */}
+        <p className="text-xs text-gray-100 font-normal">
           © {new Date().getFullYear()} KB Gym&Coaching AS -{" "}
           <Link href="/personvernerklaering/">
             <a>Personvern og Cookies</a>
           </Link>
         </p>
-        <p className="text-xs text-gray-100 ">
+        <p className="text-xs text-gray-100 font-normal">
           Drift og design:{" "}
           <Link href="https://strandos-ikt.no">
             <a>Strandos IKT</a>
--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="font-body flex flex-col h-screen justify-between">
       <Head>
         <title>KB Gym Sande</title>
         <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="stylesheet"
-          id="monsterrat_font-css"
-          href="https://fonts.googleapis.com/css?family=Montserrat%3A300%2C300i%2C400%2C500%2C600&amp;display=swap&amp;ver=5.8.1"
-          type="text/css"
-          media="all"
-        ></link>
       </Head>
-      <div className="w-full h-70vh relative">
-        <Image src="/DSC_5845.jpg" layout="fill" objectFit="cover" priority />
-      </div>
+      <main className="mb-auto">
+        <div className="w-full h-70vh relative">
+          <Image src="/DSC_5845.jpg" layout="fill" objectFit="cover" priority />
+        </div>
+      </main>
       <Footer />
     </div>
   );
